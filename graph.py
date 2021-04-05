13301f1577850a0e3377adcc054df746b3568e2b
delishaandr <icha.delishaan@gmail.com>
2021-04-05 23:45:48 +0700

    add convert matrix to weighted matrix

--- graph.py
+++ graph.py
@@ -10,10 +10,6 @@ def make_coordinates(lines):
         list_of_coordinates.append(coords_tuple)
     return list_of_coordinates
 
-def make_edge(a, b):
-    edge = tuple((a, b))
-    return edge
-
 def make_matrix(lines):
     n = int(lines[0])
     adj_matrix = []
@@ -22,16 +18,22 @@ def make_matrix(lines):
         adj_matrix.append(line)
     return adj_matrix
 
-def make_adj_list(m):
+def make_edge(a, b):
+    edge = tuple((a, b))
+    return edge
+
+# fungsi buat bikin list of edges, edge nya tuple 2 simpul
+# belom kepake di main program wkwk
+def make_edge_list(m):
     n = len(m)
-    adj_list = []
+    edge_list = []
     for i in range(0, n):
         for j in range(i, n): # asumsi semua jalan 2 arah, 
             if m[i][j] == '1':
                 edge = make_edge(str(i+1), str(j+1))
-                adj_list.append(edge)
-    return adj_list
-    
+                edge_list.append(edge)
+    return edge_list
+
 # parameternya tuple of coordinate a, dan tuple of coordinate b
 # return: haversineDistance dalam meters 
 def haversineDistance(a,b):
@@ -51,22 +53,37 @@ def haversineDistance(a,b):
     # apply formulae
     a = (pow(math.sin(dLat / 2), 2) + 
          pow(math.sin(dLon / 2), 2) * 
-             math.cos(lat1) * math.cos(lat2));
+             math.cos(lat1) * math.cos(lat2))
     rad = 6371
     c = 2 * math.asin(math.sqrt(a))
     meters = rad * c * 1000
     return meters
+
+# matriks diubah jadi matriks berbobot
+# buat matriks baru soalnya kali aja matriks yang gak berbobot masih dipake
+def convert_to_weighted(m, c):
+    wm = [] 
+    for i in range(0, len(m)):
+        line = []
+        for j in range(0, len(m)):
+            if m[i][j] == '0':
+                line.append(0) # ini diubah dari string ke int
+            else: # m[i][j] == '1'
+                line.append(haversineDistance(c[i], c[j]))
+        wm.append(line)
+    return wm
 
 # PROGRAM UTAMA
 f = open("itb.txt", "r")
 lines = f.read().splitlines()
 coordinates = make_coordinates(lines)
 matrix = make_matrix(lines)
-adj_list = make_adj_list(matrix)
-print(adj_list)
-print(coordinates)
-for line in coordinates:
-    print(line)
+wmatrix = convert_to_weighted(matrix, coordinates)
+
+for i in range(len(wmatrix)):
+    for j in range(len(wmatrix)):
+        print(wmatrix[i][j], end=' ')
+    print()
 
 # TEST HAVERSINE
-print(haversineDistance(coordinates[1],coordinates[4]))
+# print(haversineDistance(coordinates[1],coordinates[4]))
